02fd010442077b3fd2e4d4e50529b2e7275d4939
dor3Monbotz <118621248+dor3Monbotz@users.noreply.github.com>
2022-11-28 18:16:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,14 +13,14 @@ CHAT_ID = [int(crown_botz) for crown_botz in environ.get("CHAT_ID", None).split(
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "ʜᴇʟʟᴏ {mention}\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ {title}\n\nʏᴏᴜʀ ᴀᴜᴛᴏ ᴀᴘᴘʀᴏᴠᴇᴅ ʙʏ ᴍᴇ")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
-@kunal.on_message(filters.private & filters.command(["start"]))
+@crown_botz.on_message(filters.private & filters.command(["start"]))
 async def start(client: kunal, message: Message):
     approvedbot = await client.get_me() 
     button = [[ InlineKeyboardButton("ʀᴇᴘᴏ", url="https://github.com/dor3Monbotz/DoraAutoApproveBot"), InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ 📢", url="https://t.me/+OUZXn9nigZYxOWU9") ],
               [ InlineKeyboardButton("➕️ ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
     await client.send_message(chat_id=message.chat.id, text=f"**__ʜᴇʟʟᴏ {message.from_user.mention} ɪᴀᴍ ᴀᴜᴛᴏ ᴀᴘᴘʀᴏᴠᴇʀ ᴊᴏɪɴ ʀᴇǫᴜᴇsᴛ ʙᴏᴛ ᴊᴜsᴛ [ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo Sorry But Repo is now private some reasons||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
-@kunal.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+@crown_botz.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
 async def autoapprove(client: kunal, message: ChatJoinRequest):
     chat=message.chat # Chat
     user=message.from_user # User
@@ -31,4 +31,4 @@ async def autoapprove(client: kunal, message: ChatJoinRequest):
     #   print("Welcome....")
 
 print("Auto Approved Bot")
-kunal.run()
+crown_botz.run()
